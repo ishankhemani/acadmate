5ea7ae7cce285e1a868230586c92e385fc9aa23b
ishankhemani <ishankhemani1@gmail.com>
2025-05-04 09:21:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -51,10 +51,10 @@ const subjectData = {
     ],
     "EGD": [
         { title: "(unit1-solid) drawing book pdf ", video: "https://www.youtube.com/embed/zAw3FpdRlRg?si=jdjfLiCjLlvKON8A", notes: "https://drive.google.com/file/d/1itFX-s_3fDhmAUrsalRIWR1VbOjMUsdY/view?usp=drivesdk", pdf: "assets/egd_notes1.pdf" },
-        { title: "point Projection", video: "https://www.youtube.com/embed/dbPDln9FTVA?si=wycgrZFaBqOhF3je", notes: "https://drive.google.com/file/d/1itFX-s_3fDhmAUrsalRIWR1VbOjMUsdY/view?usp=drivesdk", pdf: "assets/egd_notes2.pdf" },
-        { title: "line Projection", video: "https://www.youtube.com/embed/C6A-zqYdb20?si=g8lhYb3zKkYuVDkU" , notes: "https://drive.google.com/file/d/1VL7ZJKWXPkVhkMRa6HoQ6Y79HBxwQoro/view?usp=drivesdk", pdf: "assets/egd_notes2.pdf" },
+        { title: "point Projection( or refer tikle's academy )", video: "https://www.youtube.com/embed/dbPDln9FTVA?si=wycgrZFaBqOhF3je", notes: "https://drive.google.com/file/d/1itFX-s_3fDhmAUrsalRIWR1VbOjMUsdY/view?usp=drivesdk", pdf: "assets/egd_notes2.pdf" },
+        { title: "line Projection(or refer tikle's academy)", video: "https://www.youtube.com/embed/C6A-zqYdb20?si=g8lhYb3zKkYuVDkU" , notes: "https://drive.google.com/file/d/1VL7ZJKWXPkVhkMRa6HoQ6Y79HBxwQoro/view?usp=drivesdk", pdf: "assets/egd_notes2.pdf" },
         { title: "plane", video: "https://www.youtube.com/embed/DwWlgaDEQYw?si=TCYI_mBAgz3SIHd2", notes: "https://drive.google.com/file/d/1itFX-s_3fDhmAUrsalRIWR1VbOjMUsdY/view?usp=drivesdk", pdf: "assets/egd_notes2.pdf" },
-        { title: "orthogonal projection", video: "https://www.youtube.com/embed/No2BAd569CA?si=2trVR2G0RtDUCYbU", notes: "https://drive.google.com/file/d/1VOdOvltF09nAX0DVvHAWptp0PdtBZ7BW/view?usp=drivesdk", pdf: "assets/egd_notes2.pdf" },
+        { title: "orthogonal projection(or refer tikle's academy)", video: "https://www.youtube.com/embed/No2BAd569CA?si=2trVR2G0RtDUCYbU", notes: "https://drive.google.com/file/d/1VOdOvltF09nAX0DVvHAWptp0PdtBZ7BW/view?usp=drivesdk", pdf: "assets/egd_notes2.pdf" },
         { title: "Solids", video: "https://www.youtube.com/embed/-NJ-DtjwtoA?si=8LxxGIFAmtJzAAp3" , notes: "https://drive.google.com/file/d/1GNnn2o3Wp4PVqga-3PTyLVjcTTNsPR0g/view?usp=drivesdk", pdf: "assets/egd_notes2.pdf" },
         {title:"Isometric Projection",video:"https://www.youtube.com/embed/paDG7SvpdCQ?si=yhUsexSyFUGpv_1a",notes:"https://drive.google.com/file/d/1a553M8jOgu__j8ziKnytmMcDX-8WGdYz/view?usp=drivesdk"},
         {title:"autocad",video:"https://www.youtube.com/embed/ZcGlKaGgiXo?si=Mrzac0kEsb0LFo7e",notes:"https://drive.google.com/file/d/1bNPDaBA8OOhlF0aE4ORRceor-cIEmX-X/view?usp=drivesdk" }
